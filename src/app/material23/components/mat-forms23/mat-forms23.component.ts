21419730612c04d2a2fa57752f85e57a6d59bcf2
gopal612 <gopal612@gmail.com>
2021-11-19 17:27:18 +0530

    mat forms added

---
+++ src/app/material23/components/mat-forms23/mat-forms23.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-forms23',
+  templateUrl: './mat-forms23.component.html',
+  styleUrls: ['./mat-forms23.component.css']
+})
+export class MatForms23Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/material23/material23.module.ts
+++ src/app/material23/material23.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Sidenav2Component } from './components/sidenav2/sidenav2.component';
 import { Material23RoutingModule } from './material23-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatForms23Component } from './components/mat-forms23/mat-forms23.component';
 
 
 @NgModule({
   declarations: [
-    Sidenav2Component
+    Sidenav2Component,
+    MatForms23Component
   ],
   imports: [
     CommonModule,

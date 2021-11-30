aab61bf8a1c00746b42bf4a730bf26fb29a522fd
gopal612 <gopal612@gmail.com>
2021-11-30 19:40:01 +0530

    added interceptor service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubjectsModule } from './subjects/subjects.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductGuardService } from './classes/ProductGuardService';
-
+import { HttpInterceptor23 } from './classes/http-interceptor23';
 
 
 
@@ -25,6 +25,11 @@ import { ProductGuardService } from './classes/ProductGuardService';
   ],
   providers: [
     ProductGuardService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptor23,
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent]
 })

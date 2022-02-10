33cd89fab4bd824259117e2a50c5a9638363a691
gopal-sakshi <gopal612@gmail.com>
2022-02-10 20:45:12 +0530

    practicing useFactory

--- src/app/forms12/directives/validate-email-directive14.ts
+++ src/app/forms12/directives/validate-email-directive14.ts
@@ -1,6 +1,6 @@
 /*
     Sometimes, a custom validator has dependencies so we need a way to inject them
-    Let’s say our email validator needs an EmailBlackList service, to check if the given control value is 
+    Let’s say our email validator needs an EmailBlackList service, to check if the given control value is
         not only a "valid email address"
         but also "not on our email black list"
     In an ideal world, we’d build a separate validator for checking against an email black list
@@ -16,27 +16,57 @@ import { Directive } from "@angular/core";
 import { FormControl, NG_VALIDATORS } from "@angular/forms";
 import { EmailBlackList12Service } from "../classes/email-black-list12-service";
 
+
+// directly using useValue (It works)
 function tempFunction14(c:FormControl) {
-    console.log('testing validateEmail14 - tempFunction');
+    console.log('testing validateEmail14 - tempFunction');            // This Works
     if(c.value === 'abc') {
         return {error:'dont use abc, idiot'}
     } else {
         return null;
-    }    
+    }
 }
 
-function someFunction() {
-    console.log('some function abbayi');
+// didnt work... didnt throw error
+function someFunction_with_params(c: FormControl, emailBlackListService44: EmailBlackList12Service) {
+    console.log('some function abbayi ',c.value);
+    let isInvalid = emailBlackListService44.isInvalidEmail(c.value);
+    return isInvalid ? { validateEmail: {invalid: true} } : null
+}
+
+// this works with validateEmail14Factory_temp2(); we just return this function, without much functionality
+function someFunction_temp2() {
+    console.log('some function temp2');
     return {error:'urike testing abbayi'};
     return null;
 }
-function validateEmail14Factory(emailBlackListService44: EmailBlackList12Service) {
+// this works with someFunction_temp2()
+// APPROACH 7 ---> ERROR --- Cannot read properties of undefined (reading 'validate')
+function validateEmail14Factory_temp2(emailBlackListService44:EmailBlackList12Service) {
+    return someFunction_temp2;
+}
+
+// APPROACH 5 ---> ERROR -- Cannot read properties of undefined (reading 'isInvalidEmail')
+// APPROACH 6 ---> ERROR --- Cannot read properties of undefined (reading 'validate')
+function validateEmail14Factory_temp1(emailBlackListService44:EmailBlackList12Service) {
+  return (c: FormControl, emailBlackListService44: EmailBlackList12Service) => {
+        // returns true for 'gopal612@gmail.com' & 'ped@gmail.com' ---------> because, they are blacklisted...
+        let isInvalid = emailBlackListService44.isInvalidEmail(c.value);
+        return isInvalid ? { validateEmail: {invalid: true} } : null
+    };
+}
+
+// APPROACH 1 ---> ERROR -- cant read properties of undefined (reading validate)
+// APPROACH 3 ---> its working
+const validateEmail14Factory_using_const1 = (emailBlackListService44: EmailBlackList12Service) => {
+  return someFunction_temp2;
+}
+
+// APPROACH 2 ----> ERROR -- cant read properties of undefined (reading validate)
+// APPROACH 4 ----> ERROR Cannot read properties of undefined (reading 'isInvalidEmail')
+const validateEmail14Factory_using_const2 = (emailBlackListService44: EmailBlackList12Service) => {
     console.log("testing emailValidator14");
-    return someFunction
-    return (c: FormControl) => {
-        
-        //  let EMAIL_REGEXP = /d/                  /* some regex expression */
-        
+    return (c: FormControl, emailBlackListService44: EmailBlackList12Service) => {
         // returns true for 'gopal612@gmail.com' & 'ped@gmail.com' ---------> because, they are blacklisted...
         let isInvalid = emailBlackListService44.isInvalidEmail(c.value);
         return isInvalid ? { validateEmail: {invalid: true} } : null
@@ -48,18 +78,34 @@ function validateEmail14Factory(emailBlackListService44: EmailBlackList12Service
     providers: [
         {
             provide: NG_VALIDATORS,
-            
-            
-            
-            // in validateEmail12 directive, we used 'useValue'
-            // but in validateEmail14 directive, we are forced to use 'useFactory'
-                // because we dont return the factoryFunction... rather we return what the factoryFunction returns
-            useFactory: validateEmail14Factory,
+
+
+
+
+            // useFactory: (emailBlackListService44) => {
+            //   validateEmail14Factory_using_const1(emailBlackListService44)              // APPROACH 1
+            //   // validateEmail14Factory_using_const2(emailBlackListService44)           // APPROACH 2
+            // },
+            // deps: [EmailBlackList12Service],
+
+            // useFactory: validateEmail14Factory_using_const1,                                  // APPROACH 3
+            // deps: [EmailBlackList12Service],
+
+            // useFactory: validateEmail14Factory_using_const2,                                  // APPROACH 4
+            // deps: [EmailBlackList12Service],
+
+            // useFactory: validateEmail14Factory_temp1,                                       // APPROACH 5
+            // deps: [EmailBlackList12Service],
+
+            useFactory: (emailBlackListService44) => {
+              // validateEmail14Factory_temp1(emailBlackListService44)                              // APPROACH 6
+              validateEmail14Factory_temp2(emailBlackListService44)                            // APPROACH 7
+            },
             deps: [EmailBlackList12Service],
 
             // useValue: tempFunction14,
 
-            
+
             multi: true
         }
     ]
--- src/app/life-cycle-hooks11/components/change-detection12/change-detection12.component.ts
+++ src/app/life-cycle-hooks11/components/change-detection12/change-detection12.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, NgZone, OnInit } from '@angular/core';
 import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-change-detection12',
@@ -14,10 +15,12 @@ export class ChangeDetection12Component implements OnInit {
 
   constructor(
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: any,
+    private ngZone: NgZone,
     private bottomSheetRef: MatBottomSheetRef<ChangeDetection12Component>
   ) { }
 
   ngOnInit(): void {
+    console.log("isLoaded12 is", this.isLoaded12);
     this.playerName = this.data.playerName;
     this.club = this.data.club;
 
@@ -31,8 +34,30 @@ export class ChangeDetection12Component implements OnInit {
     // }, 1000);
     const img12 = 'https://en.wikipedia.org/wiki/Elephant#/media/File:Loxodontacyclotis.jpg'
     const response = await fetch(img12);
-    this.isLoaded12 = true;
+    await this.someAsyncFunction();
+
+    this.ngZone.runOutsideAngular(()=> {
+     // this.isLoaded12 = true;                 // view not updated... because, its run outside Angular
+    });                                           // we show "not Modric"
+
+    this.isLoaded12 = true;                // view updated... we show "Modric"
+    console.log(this.isLoaded12);
   }
+
+  someAsyncFunction() {
+    var count = 10000;
+
+    setTimeout(()=> {
+      console.log("waited 5 seconds");
+    }, 5000)
+
+    return new Promise( resolve => {
+      setTimeout(()=> {
+        console.log("promise resolved after 2 seconds");
+      },2000);
+    })
+  }
+
 
   cancel() {
     this.bottomSheetRef.dismiss();

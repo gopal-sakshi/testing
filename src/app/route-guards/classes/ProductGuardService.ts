517a45023b066ea08ac942cd21966d98477db414
gopal-sakshi <gopal612@gmail.com>
2022-02-13 16:31:17 +0530

    using promises canActivate

--- src/app/route-guards/classes/ProductGuardService.ts
+++ src/app/route-guards/classes/ProductGuardService.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 
@@ -8,14 +9,14 @@ import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angul
     create a class ProductGuardService which implements canActivate
     this class must implement canActivate() method
 
-    The canActivate method must return 
+    The canActivate method must return
         either a True or a False value.
-        If it returns true, the navigation process continues. 
+        If it returns true, the navigation process continues.
         if it returns false, the navigation process stops and the user stays put.
         Guard can also return an Observable or a Promise which eventually returns a True or false
 
 
-    guards are nothing but services. 
+    guards are nothing but services.
     We need to register them with the Providers array of the Angular Module
     Guards and all other services that guard depends on ------> must be provided at the module level (bcoz angular needs them ??)
 
@@ -25,13 +26,43 @@ import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angul
 */
 
 @Injectable()
-export class ProductGuardService implements CanActivate { 
+export class ProductGuardService implements CanActivate {
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    testVariable: boolean = true;
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> | Observable<boolean> | boolean {
         console.log("canActivate");
         console.log(route);
         console.log(state);
-        return true;
-        
+
+
+        const p1 = new Promise((resolve, reject) => {
+          if(this.testVariable) {
+            setTimeout(() => {
+              console.log('waited 5 seconds');
+              resolve('route can be activated');
+            }, 5000)
+          } else {
+            setTimeout(() => {
+              console.log('waited 5 seconds in else');
+              reject('cart value must be 250');
+            }, 5000)
+          }
+        });
+
+        return p1.then(this.doStuff1).catch(this.doStuff2);
+
     }
+
+    doStuff1() : boolean {
+      console.log('ehllo dostuff 1');
+      return true;
+    }
+    doStuff2() : boolean {
+      console.log('ehllo dostuff 2');
+      return false;
+    }
+
+
+
 }
--- src/app/route-guards/components/activate23/activate23.component.ts
+++ src/app/route-guards/components/activate23/activate23.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-activate23',
   templateUrl: './activate23.component.html',
-  styleUrls: ['./activate23.component.css']
+  styleUrls: ['./activate23.component.scss']
 })
 export class Activate23Component implements OnInit {
 

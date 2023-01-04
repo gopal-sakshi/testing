fdfa2dbd5a26c5414a651c3427f363594ce47321
gopal-sakshi <gopal612@gmail.com>
2023-01-04 17:29:02 +0530

    hmr nullish

--- src/app/js-stuff12/components/js-home12/js-home12.component.ts
+++ src/app/js-stuff12/components/js-home12/js-home12.component.ts
@@ -21,11 +21,31 @@ export class JsHome12Component implements OnInit {
   
   inputToConsole:string = 'Input from parent';
   format23:string = 'ODI';
+  age23:any;
 
   constructor() { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
+
+    let random1 = Math.random();
+    console.log(random1);
+
+    // nullish coalescing operator (??) 
+      // a logical operator 
+      // that returns its right-hand side operand when its left-hand side operand is null or undefined
+      // and otherwise returns its left-hand side operand.
+
+    // age23 can be null (or) undefined (or) 'valid age'
+    if (random1 > 0.00 && random1 < 0.33) this.age23 = null;
+    else if (random1 < 0.66) this.age23 = undefined;
+    else this.age23 = random1;
+    
+    // if age23 is null/undefined =================> then blah23 = string
+    // elseif age23 is valid (ie above 0.66) ======> then blah23 = 0.76 (or) 0.84
+    var blah23 = this.age23 ?? 'if age23 is null/undefined, blah23 is this value';
+    console.log(blah23);
   }
+
 
   changeInputToConsole23() {
     this.inputToConsole = 'Gonzalo Higuain';
